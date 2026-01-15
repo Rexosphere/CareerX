96816950e19464da55f03843eb11ef73ceee88b4
kalana-pankaja <kalanaliyanage22@gmail.com>
2026-01-15 22:16:59 +0530

    feat: Update hero section with a new background , improved overlay, and text styling.

--- resources/views/livewire/home/hero.blade.php
+++ resources/views/livewire/home/hero.blade.php
@@ -6,11 +6,14 @@ new class extends Component {
     //
 }; ?>
 
-<div class="hero min-h-125 bg-base-300 relative overflow-hidden">
-    <!-- Background Image with Overlay -->
-    <div class="absolute inset-0 z-0 bg-cover bg-center opacity-40"
-        style="background-image: url('https://lh3.googleusercontent.com/aida-public/AB6AXuBNgz6ZCdVAQTTIe5xlquGLVkPrXvvmaxov73wcbPQxTLA5SguLCGnZ6ERIWbBFO3pkoWULR_JEG81Yehuay5a_3W2dsVGqjy_bPU_P-J0rpwAxkVq979ccczIQdp-RweEv0LDxjZhWrnuGLoZyhIXYs9HrHzByzhZOtIOdHwVb8zvkdPTVOAdFeIPbjDF_gBrjr3h8efe_-E9KwVER74O-b9wJ3nBEwBqgWOKo4ZpCCS8P7rPxhvuujOoZQSsjEMFOnBxlH5iUsaw');">
+<div class="hero min-h-125 relative overflow-hidden">
+    <!-- Background Image -->
+    <div class="absolute inset-0 z-0 bg-cover opacity-70"
+        style="background-image: url('{{ asset('hero.avif') }}'); background-position: center 15%;">
     </div>
+    
+    <!-- Dark overlay for better text visibility -->
+    <div class="absolute inset-0 z-[5] bg-black/40"></div>
 
     <div class="hero-content text-center z-10 relative">
         <div class="max-w-3xl">
@@ -18,7 +21,7 @@ new class extends Component {
                 University of Moratuwa
             </div> -->
             <h1 class="text-5xl md:text-6xl font-bold text-base-content mb-6">
-                Connect with Future <span class="text-info">Innovators</span>
+                Connect with Future <span class="text-primary font-extrabold">Innovators</span>
             </h1>
             <p class="text-lg md:text-xl text-base-content/80 mb-8">
                 The official bridge between University of Moratuwa's top engineering talent and the industry's leading
